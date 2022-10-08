7efc5e63353842162c59bd5ea6b02ed5d0e38545
Kalynovsky Valentin <nakama3942@gmail.com>
2022-10-09 01:00:54 +0300

    Extracted the port check function into a separate file and added validation of the ports input field

--- main.py
+++ main.py
@@ -1,21 +1,9 @@
-import socket
 import sys
 
 from PyQt6 import QtWidgets
 from PyQt6.QtWidgets import QApplication
 
 from ui.scanport import ScanPort
-
-
-def is_port_open(host, port):
-    s = socket.socket()
-    try:
-        s.connect((host, port))  # Connection attempt
-        # s.settimeout(0.05)  # Timeout for a little more speed
-    except:
-        return False  # Port is closed
-    else:
-        return True  # Port is opened
 
 
 if __name__ == '__main__':
--- ui/scanport.py
+++ ui/scanport.py
@@ -14,6 +14,8 @@
 
 from PyQt6 import QtWidgets
 from PyQt6.QtWidgets import QApplication, QMainWindow
+from PyQt6.QtCore import QRegularExpression
+from PyQt6.QtGui import QRegularExpressionValidator
 
 from ui.raw.ui_scanport import Ui_ScanPort
 
@@ -23,8 +25,22 @@ class ScanPort(QMainWindow, Ui_ScanPort):
         super(ScanPort, self).__init__()
         self.setupUi(self)
 
-        # # It's a tracking of button clicks in the window
-        # self.buttSend.released.connect(lambda: self.buttSend_Released())
+        self.port_list: list = []
+
+        # Створюю валідатор для поля вводу списку портів
+        rx = QRegularExpression('^(((4915[0-2]|491[0-4][0-9]|49[0][0-9][0-9]|4[0-8][0-9][0-9][0-9]|[0-3]?[0-9][0-9][0-9][0-9])(-)(4915[0-2]|491[0-4][0-9]|49[0][0-9][0-9]|4[0-8][0-9][0-9][0-9]|[0-3]?[0-9][0-9][0-9][0-9])(,))|((4915[0-2]|491[0-4][0-9]|49[0][0-9][0-9]|4[0-8][0-9][0-9][0-9]|[0-3]?[0-9][0-9][0-9][0-9])(,))){0,}$')
+        validator = QRegularExpressionValidator(rx1, self)
+        self.linePorts.setValidator(validator)
+
+        # It's a tracking of button clicks in the window
+        self.buttScan.clicked.connect(self.buttScan_Clicked)
         # self.listLetters.activated.connect(lambda: self.listLetters_Activated(self.listLetters.currentRow()))
         # self.actionRelogin.triggered.connect(lambda: self.actionRelogin_Triggered())
         # self.actionRedownload.triggered.connect(lambda: self.actionRedownload_Triggered())
+
+    def buttScan_Clicked(self):
+        # 6351,6352,1850,49152,8000-9000,987
+        self.port_list = self.linePorts.text().split(',')
+        for i in range(len(self.port_list)):
+            self.port_list[i] = self.port_list[i].split('-')
+        self.textResult.setText(str(self.port_list))  # Тест роботи сплітів
